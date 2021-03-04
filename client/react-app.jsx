0ae5b8503bcb61e4656b0c9b81f792c791eb7809
XBOCTATYK <sha-ej@yandex.ru>
2021-03-05 01:39:20 +0300

    feat(*): removed console log

--- client/react-app.jsx
+++ client/react-app.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import { GameList } from '../components/game-list/game-list';
 
-console.log('eeee')
-
 export function createApp() {
     ReactDOM.hydrate(<GameList />, document.getElementById('app'))
 }
